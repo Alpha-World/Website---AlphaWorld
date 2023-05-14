14663356e05664fb5d95335c2107dc1323954d7c
kichotee <pt.timone@gmail.com>
2023-05-14 21:26:26 +0100

    new updates

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,27 @@
 <template>
-    <!-- navbar configuration -->
-    <div class="h-[3.69rem] flex justify-center text-xl items-center w-full bg-[#0027A6] text-white ">
-        <img class="w-[9.5rem]" src="/images/Logo-name.png" alt="">
+  <!-- navbar configuration -->
+  <div
+    class="h-[3.69rem] flex justify-between text-xl items-center w-full bg-[#0027A6] text-white"
+  >
+    <div class="pl-4">
+      
+      <!-- Instagram -->
+      <a target="_blank" href="https://www.linkedin.com/company/alphatech7/">
+        <img class="w-4 md:w-auto" src="/images/telegram.png" alt="" />
+      </a>
     </div>
+    <img class="w-[9.5rem]" src="/images/Logo-name.png" alt="" />
+    <div class="pr-4 ">
+        <a
+        target="_blank"
+        href="https://chat.whatsapp.com/Iz4YdJb4wko9s6lhM81858"
+      >
+        <img class="w-4 md:w-auto" src="/images/Whatsapp.png" alt="" />
+      </a>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
