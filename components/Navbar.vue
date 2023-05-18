7d731ce9a36941d9a6fb49b23270cb27d3099d92
kichotee <pt.timone@gmail.com>
2023-05-18 10:08:34 +0100

    update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,22 +4,21 @@
     class="h-[3.69rem] flex justify-between text-xl items-center w-full bg-[#0027A6] text-white"
   >
     <div class="pl-4">
-      
       <!-- Telegram -->
       <a target="_blank" href="https://www.linkedin.com/company/alphatech7/">
-        <img class="w-4 md:w-auto" src="/images/telegram.png" alt="" />
+        <img class="w-4 md:w-auto" src="/images/Linkedin-white.png" alt="" />
       </a>
     </div>
     <img class="w-[9.5rem]" src="/images/Logo-name.png" alt="" />
 
-    <div class="pr-4 ">
+    <div class="pr-4">
       <!-- Whatsapp -->
 
-        <a
+      <a
         target="_blank"
-        href="https://chat.whatsapp.com/Iz4YdJb4wko9s6lhM81858"
+        href="https://instagram.com/alphaworldapp?igshid=YmMyMTA2M2Y="
       >
-        <img class="w-4 md:w-auto" src="/images/whatsapp.png" alt="" />
+        <img class="w-4 md:w-auto" src="/images/Instagram-white.png" alt="" />
       </a>
     </div>
   </div>
